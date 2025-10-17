f8191f3956b0b0c6d35286d195de358cebe480a2
Sai Mohan <mokshithsai281@gmail.com>
2025-10-17 09:19:09 +0530

    Fifth Commit

--- ecobazaar/src/main/java/com/ecobazaar/ecobazaar/controller/OrderController.java
+++ ecobazaar/src/main/java/com/ecobazaar/ecobazaar/controller/OrderController.java
@@ -2,7 +2,11 @@ package com.ecobazaar.ecobazaar.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.ecobazaar.ecobazaar.model.Order;
 import com.ecobazaar.ecobazaar.service.OrderService;
@@ -11,21 +15,19 @@ import com.ecobazaar.ecobazaar.service.OrderService;
 @RequestMapping("/api/orders")
 public class OrderController {
 
-    private final OrderService orderService;
-    public OrderController(OrderService orderService) {
-        this.orderService = orderService;
-    }
-
-    // ✅ Checkout (create new order)
-    @PostMapping("/checkout/{userId}")
-    public Order checkout(@PathVariable Long userId) {
-        return orderService.checkout(userId);
-    }
-
-
-    // ✅ View past orders
-    @GetMapping("/{userId}")
-    public List<Order> getUserOrders(@PathVariable Long userId) {
-        return orderService.getOrdersByUser(userId);
-    }
+	private final OrderService orderService;
+	
+	public OrderController(OrderService orderService) {
+		this.orderService = orderService;
+	}
+	
+	@PostMapping("/checkout/{userId}")
+	public Order checkout(@PathVariable Long userId) {
+		return orderService.checkout(userId);
+	}
+	
+	@GetMapping("/{userId}")
+	public List<Order> getUserOrder(@PathVariable Long userId){
+		return orderService.getOrdersByUserId(userId);
+	}
 }
--- ecobazaar/src/main/java/com/ecobazaar/ecobazaar/repository/OrderRepository.java
+++ ecobazaar/src/main/java/com/ecobazaar/ecobazaar/repository/OrderRepository.java
@@ -1,9 +1,13 @@
 package com.ecobazaar.ecobazaar.repository;
 
 import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.ecobazaar.ecobazaar.model.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    List<Order> findByUserId(Long userId);
+	
+	List<Order> findByUserId(Long userId);
+
 }
