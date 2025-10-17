1dbc290c1c23f899e2a809d8dbef530b9efc3909
Sai Mohan <mokshithsai281@gmail.com>
2025-10-17 18:19:47 +0530

    Sixth Commit

--- ecobazaar/src/main/java/com/ecobazaar/ecobazaar/service/CartService.java
+++ ecobazaar/src/main/java/com/ecobazaar/ecobazaar/service/CartService.java
@@ -13,55 +13,55 @@ import com.ecobazaar.ecobazaar.repository.ProductRepository;
 
 @Service
 public class CartService {
-	
-	private final CartRepository cartRepository;
-	private final ProductRepository productRepository;
-	
-	public CartService(CartRepository cartRepository, ProductRepository productRepository) {
-		this.cartRepository = cartRepository;
-		this.productRepository = productRepository;
-	}
-	
-	public CartItem addToCart(CartItem cartItem) {
-		return cartRepository.save(cartItem);
-	}
-	
+    
+    private final CartRepository cartRepository;
+    private final ProductRepository productRepository;
+    
+    public CartService(CartRepository cartRepository, ProductRepository productRepository) {
+        this.cartRepository = cartRepository;
+        this.productRepository = productRepository;
+    }
+    
+    public CartItem addToCart(CartItem cartItem) {
+        return cartRepository.save(cartItem);
+    }
 
-	public CartSummaryDTO getCartSummary(Long userId) {
-	    List<CartItem> cartItems = cartRepository.findByUserId(userId);
+    public CartSummaryDTO getCartSummary(Long userId) {
+        List<CartItem> cartItems = cartRepository.findByUserId(userId);
 
-	    double totalPrice = 0;
-	    double totalCarbon = 0;
-	    String ecoSuggestion = null;
+        double totalPrice = 0;
+        double totalCarbon = 0;
+        String ecoSuggestion = null;
 
-	    for (CartItem item : cartItems) {
-	        Product product = productRepository.findById(item.getProductId())
-	                .orElseThrow(() -> new RuntimeException("Product not found: " + item.getProductId()));
+        for (CartItem item : cartItems) {
+            Product product = productRepository.findById(item.getProductId())
+                    .orElseThrow(() -> new RuntimeException("Product not found: " + item.getProductId()));
 
-	        totalPrice += product.getPrice() * item.getQuantity();
-	        totalCarbon += product.getCarbonImpact() * item.getQuantity();
+            totalPrice += product.getPrice() * item.getQuantity();
+            totalCarbon += product.getCarbonImpact() * item.getQuantity();
 
-	        if (Boolean.FALSE.equals(product.getEcoCertified())) {
-	            Optional<Product> ecoAlt = productRepository
-	                    .findFirstByEcoCertifiedTrueAndNameContainingIgnoreCase(product.getName());
+            if (Boolean.FALSE.equals(product.getEcoCertified())) {
+                String[] words = product.getName().split(" ");
+                String keyword = words[words.length - 1].replaceAll("[^a-zA-Z]", ""); 
 
-	            if (ecoAlt.isPresent()) {
-	                double saved = product.getCarbonImpact() - ecoAlt.get().getCarbonImpact();
+                Optional<Product> ecoAlt = productRepository
+                        .findFirstByEcoCertifiedTrueAndNameContainingIgnoreCase(keyword);
 
-	                if (saved > 0.5) {
-	                    ecoSuggestion = "💡 Switch to " + ecoAlt.get().getName()
-	                            + " and save " + saved + " kg CO₂!";
-	                }
-	            }
-	        }
-	    }
+                if (ecoAlt.isPresent()) {
+                    double saved = product.getCarbonImpact() - ecoAlt.get().getCarbonImpact();
 
-	    return new CartSummaryDTO(cartItems, totalPrice, totalCarbon, ecoSuggestion);
-	}
+                    if (saved > 0.5) {
+                        ecoSuggestion = "💡 Switch to " + ecoAlt.get().getName()
+                                + " and save " + String.format("%.2f", saved) + " kg CO₂!";
+                    }
+                }
+            }
+        }
 
-	
-	public void removeFromCart(Long id) {
-		cartRepository.deleteById(id);
-	}
+        return new CartSummaryDTO(cartItems, totalPrice, totalCarbon, ecoSuggestion);
+    }
 
+    public void removeFromCart(Long id) {
+        cartRepository.deleteById(id);
+    }
 }
