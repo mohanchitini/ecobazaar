23e2eb92d8cb1108f3c6e99c0248189b062712aa
Sai Mohan <mokshithsai281@gmail.com>
2025-10-24 17:47:22 +0530

    Security Commit

---
+++ ecobazaar/src/main/java/com/ecobazaar/ecobazaar/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.ecobazaar.ecobazaar.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterConfig(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/auth/register", "/api/auth/login").permitAll()
+                .anyRequest().authenticated()
+            );
+
+        return http.build();
+    }
+    
+//  @Bean
+//    public InMemoryUserDetailsManager userDetailsService() {
+//    	UserDetails admin = User.withUsername("admin")
+//    			.password("{noop}admin123")
+//    			.roles("ADMIN")
+//    			.build();
+//    	
+//    	return new InMemoryUserDetailsManager(admin);
+//    }
+}
