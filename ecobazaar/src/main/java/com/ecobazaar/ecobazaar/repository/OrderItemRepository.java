ea27fad13ee10ffa826f47e00294d8c545d12329
Sai Mohan <mokshithsai281@gmail.com>
2025-11-02 11:57:40 +0530

    Bug Fix

--- ecobazaar/src/main/java/com/ecobazaar/ecobazaar/repository/OrderItemRepository.java
+++ ecobazaar/src/main/java/com/ecobazaar/ecobazaar/repository/OrderItemRepository.java
@@ -4,18 +4,18 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.ecobazaar.ecobazaar.model.OrderItem;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-	
-	@Query("SELECT oi from ORDERITEM oi JOIN Product p ON oi.productId = p.id WHERE p.sellerId = :sellerId")
-	List<OrderItem> findBySellerId(Long sellerId);
-	
-	@Query("SELECT SUM(oi.quantity* p.price) FROM OrderItem oi Join Product p On oi.productId = p.id where p.sellerid = :sellerId")
-	Double getTotalRevenueBySeller(Long sellerId);
-	
-	@Query("SELECT SUM(oi.quantity* p.carbonImpact) FROM OrderItem oi Join Product p On oi.productId = p.id where p.sellerid = :sellerId")
-	Double getTotalCarbonBySeller(Long sellerId);
 
+    @Query("SELECT oi FROM OrderItem oi JOIN Product p ON oi.productId = p.id WHERE p.sellerId = :sellerId")
+    List<OrderItem> findBySellerId(@Param("sellerId") Long sellerId);
+
+    @Query("SELECT SUM(oi.quantity * p.price) FROM OrderItem oi JOIN Product p ON oi.productId = p.id WHERE p.sellerId = :sellerId")
+    Double getTotalRevenueBySeller(@Param("sellerId") Long sellerId);
+
+    @Query("SELECT SUM(oi.quantity * p.carbonImpact) FROM OrderItem oi JOIN Product p ON oi.productId = p.id WHERE p.sellerId = :sellerId")
+    Double getTotalCarbonBySeller(@Param("sellerId") Long sellerId);
 }
